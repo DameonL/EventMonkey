243ca70541f34e07ef056370961beb1851de1de7
Dameon Laird <dameonlair@gmail.com>
2023-02-14 10:35:52 -0800

    Added announcement configuration

--- src/EventMonkeyConfiguration.ts
+++ src/EventMonkeyConfiguration.ts
@@ -3,15 +3,23 @@ import { Client, GuildScheduledEventEntityType } from "discord.js";
 export interface EventMonkeyConfiguration {
   commandName: string;
   discordClient?: Client;
+  timeZone?: string;
   eventTypes: {
     name: string;
-    channelId: string;
+    channel: string;
+    announcement?: {
+      channel?: string | string[];
+      beforeStart?: number;
+      onStart?: boolean;
+      message?: string;
+    }
   }[];
-  editingTimeoutInMinutes: number;
+  editingTimeout: number;
+  closeThreadsAfter?: number;
   allowedEntityTypes?: GuildScheduledEventEntityType[];
   roleIds?: {
     allowed?: string[],
     denied?: string[]
-  }
+  };
 }
 
