c54e7f9b68a9b88f2e72b82d741737c0913ef9f1
Dameon Laird <dameonlair@gmail.com>
2023-03-04 11:54:24 -0800

    Always include attendance buttons in edit

--- src/EventCreators.ts
+++ src/EventCreators.ts
@@ -1,11 +1,11 @@
 import {
-    ActionRowBuilder,
-    AttachmentBuilder,
-    ButtonBuilder,
-    Guild,
-    GuildScheduledEventEntityType,
-    Message,
-    ThreadChannel,
+  ActionRowBuilder,
+  AttachmentBuilder,
+  ButtonBuilder,
+  Guild,
+  GuildScheduledEventEntityType,
+  Message,
+  ThreadChannel,
 } from "discord.js";
 import { attendanceButtons } from "./Content/Component/attendanceButtons";
 import { attendeesToEmbed } from "./Content/Embed/attendees";
@@ -16,108 +16,87 @@ import Time from "./Utility/Time";
 import { resolveChannelString } from "./Utility/resolveChannelString";
 
 export default {
-    createGuildScheduledEvent,
-    createThreadChannelEvent,
+  createGuildScheduledEvent,
+  createThreadChannelEvent,
 };
 
-async function createGuildScheduledEvent(
-    event: EventMonkeyEvent,
-    guild: Guild,
-    thread: ThreadChannel
-) {
-    const eventToSubmit = {} as any;
-    eventToSubmit.description = `${event.description}\nDiscussion: ${
-        thread.url
-    }\nHosted by: ${event.author.toString()}`;
-    eventToSubmit.name = `${event.name} hosted by ${event.author.username}`;
-    eventToSubmit.entityType = event.entityType;
-    if (event.entityType !== GuildScheduledEventEntityType.External) {
-        eventToSubmit.channel = event.entityMetadata.location;
-    } else {
-        eventToSubmit.entityMetadata = event.entityMetadata;
-    }
+async function createGuildScheduledEvent(event: EventMonkeyEvent, guild: Guild, thread: ThreadChannel) {
+  const eventToSubmit = {} as any;
+  eventToSubmit.description = `${event.description}\nDiscussion: ${thread.url}\nHosted by: ${event.author.toString()}`;
+  eventToSubmit.name = `${event.name} hosted by ${event.author.username}`;
+  eventToSubmit.entityType = event.entityType;
+  if (event.entityType !== GuildScheduledEventEntityType.External) {
+    eventToSubmit.channel = event.entityMetadata.location;
+  } else {
+    eventToSubmit.entityMetadata = event.entityMetadata;
+  }
 
-    eventToSubmit.scheduledStartTime = event.scheduledStartTime;
-    if (!event.scheduledEndTime) {
-        const endTime = new Date(eventToSubmit.scheduledStartTime);
-        endTime.setHours(endTime.getHours() + event.duration);
-        eventToSubmit.scheduledEndTime = endTime;
-    } else {
-        eventToSubmit.scheduledEndTime = event.scheduledEndTime;
-    }
+  eventToSubmit.scheduledStartTime = event.scheduledStartTime;
+  if (!event.scheduledEndTime) {
+    const endTime = new Date(eventToSubmit.scheduledStartTime);
+    endTime.setHours(endTime.getHours() + event.duration);
+    eventToSubmit.scheduledEndTime = endTime;
+  } else {
+    eventToSubmit.scheduledEndTime = event.scheduledEndTime;
+  }
 
-    eventToSubmit.image = event.image;
-    eventToSubmit.privacyLevel = event.privacyLevel;
-    const scheduledEvent = await (event.scheduledEvent
-        ? guild.scheduledEvents.edit(event.scheduledEvent.id, eventToSubmit)
-        : guild.scheduledEvents.create(eventToSubmit));
+  eventToSubmit.image = event.image;
+  eventToSubmit.privacyLevel = event.privacyLevel;
+  const scheduledEvent = await (event.scheduledEvent
+    ? guild.scheduledEvents.edit(event.scheduledEvent.id, eventToSubmit)
+    : guild.scheduledEvents.create(eventToSubmit));
 
-    return scheduledEvent;
+  return scheduledEvent;
 }
 
 async function createThreadChannelEvent(event: EventMonkeyEvent, guild: Guild) {
-    const scheduledEndTime = new Date(event.scheduledStartTime);
-    scheduledEndTime.setHours(scheduledEndTime.getHours() + event.duration);
-    const targetChannel = await resolveChannelString(
-        event.discussionChannelId,
-        guild
+  const scheduledEndTime = new Date(event.scheduledStartTime);
+  scheduledEndTime.setHours(scheduledEndTime.getHours() + event.duration);
+  const targetChannel = await resolveChannelString(event.discussionChannelId, guild);
+
+  if (!targetChannel) throw new Error(`Unable to resolve ID "${event.discussionChannelId}" to a channel.`);
+
+  if (!("threads" in targetChannel))
+    throw new Error(
+      `Channel with ID ${event.discussionChannelId} is of type "${targetChannel.type}". The discussion channel needs to be able to have threads.`
     );
 
-    if (!targetChannel)
-        throw new Error(
-            `Unable to resolve ID "${event.discussionChannelId}" to a channel.`
-        );
+  const threadName = `${Time.getTimeString(event.scheduledStartTime)} - ${event.name} hosted by ${
+    event.author.username
+  }`;
 
-    if (!("threads" in targetChannel))
-        throw new Error(
-            `Channel with ID ${event.discussionChannelId} is of type "${targetChannel.type}". The discussion channel needs to be able to have threads.`
-        );
+  const threadMessage: any = {
+    embeds: [await eventEmbed(event, guild), attendeesToEmbed(event.attendees)],
+    files: event.image ? [new AttachmentBuilder(event.image)] : undefined,
+  };
 
-    const threadName = `${Time.getTimeString(event.scheduledStartTime)} - ${
-        event.name
-    } hosted by ${event.author.username}`;
+  let channelMessage: Message | undefined;
+  if (event.threadChannel) {
+    channelMessage = await getEventDetailsMessage(event.threadChannel);
+    if (!channelMessage) throw new Error("Unable to get channel message from thread.");
 
-    const threadMessage: any = {
-        embeds: [
-            await eventEmbed(event, guild),
-            attendeesToEmbed(event.attendees),
-        ],
-        components: new Array<ActionRowBuilder<ButtonBuilder>>(),
-        files: event.image ? [new AttachmentBuilder(event.image)] : undefined,
-    };
-
-    let channelMessage: Message | undefined;
-    if (event.threadChannel) {
-        channelMessage = await getEventDetailsMessage(event.threadChannel);
-        if (!channelMessage)
-            throw new Error("Unable to get channel message from thread.");
-
-        threadMessage.components = [
-            attendanceButtons(event, channelMessage.id),
-        ];
-        await event.threadChannel.setName(threadName);
-    } else {
-        if (event.image) {
-            const imageAttachment = new AttachmentBuilder(event.image);
-            threadMessage.files = [imageAttachment];
-        }
-
-        const threadChannel = await targetChannel.threads.create({
-            name: threadName,
-            message: threadMessage,
-        });
-        event.threadChannel = threadChannel;
-        await Listeners.listenForButtonsInThread(threadChannel);
-
-        channelMessage = (await threadChannel.messages.fetch()).at(0);
-        if (!channelMessage)
-            throw new Error(
-                "Unable to get channel message from freshly created thread."
-            );
-
-        await channelMessage.pin();
+    await event.threadChannel.setName(threadName);
+  } else {
+    if (event.image) {
+      const imageAttachment = new AttachmentBuilder(event.image);
+      threadMessage.files = [imageAttachment];
     }
-    await channelMessage.edit(threadMessage);
 
-    return event.threadChannel;
+    const threadChannel = await targetChannel.threads.create({
+      name: threadName,
+      message: threadMessage,
+    });
+    event.threadChannel = threadChannel;
+    await Listeners.listenForButtonsInThread(threadChannel);
+
+    channelMessage = (await threadChannel.messages.fetch()).at(0);
+    if (!channelMessage) throw new Error("Unable to get channel message from freshly created thread.");
+
+    await channelMessage.pin();
+    threadChannel.members.add(event.author.id);
+  }
+  threadMessage.components = [attendanceButtons(event, channelMessage.id)];
+  await channelMessage.edit(threadMessage);
+
+  return event.threadChannel;
 }
