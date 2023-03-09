e934dcde3ac68c78eb3d3d532445be257d04f4c8
Dameon Laird <dameonlair@gmail.com>
2023-03-09 15:36:40 -0800

    Use updated types. Corrected issue when event didn't have image.

--- src/Content/Embed/eventAnnouncement.ts
+++ src/Content/Embed/eventAnnouncement.ts
@@ -1,52 +1,45 @@
-import { APIEmbed, APIEmbedField } from "discord.js";
+import { APIEmbed, APIEmbedField, GuildScheduledEventEntityType } from "discord.js";
 import { EventMonkeyEvent } from "../../EventMonkeyEvent";
 import Time from "../../Utility/Time";
 
-export default function eventAnnouncement(
-  event: EventMonkeyEvent,
-  timeBeforeStart?: number
-) {
+export default function eventAnnouncement(event: EventMonkeyEvent, timeBeforeStart?: number) {
   var idString = `Event ID: ${event.id}`;
 
   const startingString = timeBeforeStart
-    ? `will be starting in ${Math.round(
-        timeBeforeStart / Time.toMilliseconds.minutes(1)
-      )} minutes`
+    ? `will be starting in ${Math.round(timeBeforeStart / Time.toMilliseconds.minutes(1))} minutes`
     : `is starting now`;
 
   const announcementEmbed: APIEmbed = {
     title: `${timeBeforeStart ? "Upcoming " : ""}Event Reminder - ${Time.getTimeString(event.scheduledStartTime)}`,
-    description: `The event "${
-      event.name
-    }" hosted by ${event.author.toString()} ${startingString}!`,
+    description: `The event "${event.name}" hosted by ${event.author.toString()} ${startingString}!`,
     footer: {
       text: idString,
     },
-    thumbnail: { url: event.image },
   };
+
+  if (event.image) {
+    announcementEmbed.thumbnail = { url: event.image };
+  }
 
   const announcementFields: APIEmbedField[] = [
     {
       name: "Event Link",
-      value:
-        event.scheduledEvent?.toString() ??
-        event.threadChannel?.toString() ??
-        "",
+      value: event.scheduledEvent?.toString() ?? event.threadChannel?.toString() ?? "",
     },
   ];
 
   announcementEmbed.fields = announcementFields;
 
-  if (event.channel)
+  if (event.entityType !== GuildScheduledEventEntityType.External) {
     announcementFields.push({
       name: "Channel",
       value: event.channel.toString(),
     });
-  else
+  } else {
     announcementFields.push({
       name: "Location",
-      value: event.entityMetadata?.location ?? "",
+      value: event.entityMetadata.location ?? "",
     });
-
+  }
   return announcementEmbed;
 }
