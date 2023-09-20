3428fb97de9d2fa270424674979a4ef0a1e370ec
Dameon Laird <dameonlair@gmail.com>
2023-09-20 15:13:42 -0700

    Added maxAttendees check

--- src/ButtonHandlers/EventButtonHandlers.ts
+++ src/ButtonHandlers/EventButtonHandlers.ts
@@ -1,15 +1,34 @@
 import { ButtonInteraction, ChannelType } from "discord.js";
-import {
-  attendeesToEmbed,
-  getAttendeesFromMessage,
-} from "../Content/Embed/attendees";
-import { getEventDetailsEmbed } from "../Content/Embed/eventEmbed";
+import { attendeesToEmbed, getAttendeesFromMessage } from "../Content/Embed/attendees";
+import { deseralizeEventEmbed, getEventDetailsEmbed } from "../Content/Embed/eventEmbed";
+import Configuration from "../Configuration";
+import logger from "../Logger";
 
 const buttonHandlers: {
   [handlerName: string]: (interaction: ButtonInteraction) => Promise<void>;
 } = {
   attending: async (interaction: ButtonInteraction) => {
     let attendees = getAttendeesFromMessage(interaction.message);
+    if (
+      !Configuration.current.discordClient ||
+      (interaction.message.channel.type !== ChannelType.PublicThread &&
+        interaction.message.channel.type !== ChannelType.PrivateThread)
+    ) {
+      return;
+    }
+
+    const monkeyEvent = await deseralizeEventEmbed(interaction.message.channel, Configuration.current.discordClient);
+    if (!monkeyEvent) {
+      logger.error("Couldn't get event from interaction channel.");
+      await interaction.editReply("Sorry, something went wrong! Please contact an administrator.");
+      return;
+    }
+
+    if (monkeyEvent.maxAttendees && attendees.length >= monkeyEvent.maxAttendees) {
+      await interaction.editReply("Sorry, this event has the maximum number of attendees.");
+      return;
+    }
+
     if (attendees.includes(interaction.user.id)) {
       interaction.editReply({
         content: "It looks like you're already attending!",
@@ -36,9 +55,7 @@ const buttonHandlers: {
     const eventEmbed = await getEventDetailsEmbed(interaction.message);
     let attendees = getAttendeesFromMessage(interaction.message);
 
-    const eventAuthorId = eventEmbed.author?.name.match(
-      /(?<=\().*(?=\)$)/i
-    )?.[0] as string;
+    const eventAuthorId = eventEmbed.author?.name.match(/(?<=\().*(?=\)$)/i)?.[0] as string;
     if (eventAuthorId === interaction.user.id) {
       await interaction.editReply({
         content: `Hey, you can't leave your own event! If you want to cancel your event, use the edit event command, and hit the red "Cancel" button.`,
